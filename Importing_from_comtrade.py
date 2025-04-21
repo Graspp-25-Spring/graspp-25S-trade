982e67bb46973ae99a3b5f1d5163ab61b5d8fa9e
Gerwin-He <gerwin.he@hotmail.de>
2025-04-21 14:24:08 +0900

    Update Importing_from_comtrade.py

--- Importing_from_comtrade.py
+++ Importing_from_comtrade.py
@@ -7,10 +7,9 @@ def download_comtrade_trade_total(url):
     url = "https://comtradeplus.un.org/TradeFlow?Frequency=A&Flows=X&CommodityCodes=TOTAL&Partners=251&Reporter"
     print(f"Fetching Comtrade data from: {url}")
     #df = pd.read_stata(url)
-    df = pd.read_csv(r"C:\Users\Gerwin\Documents\Tokyo university\1 Data Science\datasets\COMTRADE_TotalTrade.csv", encoding="latin1")
+    df = pd.read_csv(r"C:\Users\Gerwin\Documents\Tokyo university\1 Data Science\graspp-25S-trade\datasets\COMTRADE_TotalTrade.csv", encoding="latin1")
     return df
 
 url_comtrade = "https://comtradeplus.un.org/TradeFlow?Frequency=A&Flows=X&CommodityCodes=TOTAL&Partners=251&Reporter"
 
 df_comtrade_raw = download_comtrade_trade_total(url_comtrade)
-
